921e70c59f4798522abe889c5da016f074ad957b
Andrews Medina <andrewsmedina@gmail.com>
2016-08-09 16:52:47 -0300

    support custom className

---
+++ src/switch-button.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export default class SwitchButton extends Component {
+  render() {
+    return (
+      <div className={this.props.className}></div>
+    )
+  }
+}
