64cc2058deb57b3fa62c7b0a60812340ee3de82c
Andrews Medina <andrewsmedina@gmail.com>
2016-08-10 11:38:54 -0300

    add a way to customize name, checked and onChange

--- src/__tests__/switch-button-test.js
+++ src/__tests__/switch-button-test.js
@@ -5,7 +5,18 @@ import SwitchButton from '../switch-button';
 describe('<SwitchButton />', () => {
   it('should accept className', () => {
     const wrapper = shallow(<SwitchButton className="foobar" />);
-
     expect(wrapper.hasClass('foobar')).toBe(true);
+  });
+
+  it('should be checked', () => {
+    const wrapper = shallow(<SwitchButton checked />);
+    const input = wrapper.find("input");
+    expect(input.prop("checked")).toBe(true);
+  });
+
+  it('should accept name', () => {
+    const wrapper = shallow(<SwitchButton name="myname" />);
+    const input = wrapper.find("input");
+    expect(input.prop("name")).toBe("myname");
   });
 });
--- src/switch-button.js
+++ src/switch-button.js
@@ -1,9 +1,36 @@
 import React, { Component } from "react";
 
 export default class SwitchButton extends Component {
+  constructor(props) {
+    super(props);
+
+    this.onChange = this.props.onChange.bind(this);
+  }
+
   render() {
     return (
-      <div className={this.props.className}></div>
+      <div className={this.props.className}>
+      <label></label>
+      <input
+      onChange={this.props.onChange}
+      checked={this.props.checked}
+      name={this.props.name}
+      type="checkbox"
+      value="1"
+      />
+      </div>
     )
   }
+}
+
+SwitchButton.propTypes = {
+  name           : React.PropTypes.string,
+  checked        : React.PropTypes.bool,
+  onChange       : React.PropTypes.func
+}
+
+SwitchButton.defaultProps = {
+  name           : 'switch-button',
+  checked        : null,
+  onChange       : () => {}
 }
