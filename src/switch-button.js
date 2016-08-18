7404ea9a5541aaa5e2df3fe397d34c39d39bcbea
Andrews Medina <andrewsmedina@gmail.com>
2016-08-18 18:26:52 -0300

    add behaviours

--- src/switch-button.js
+++ src/switch-button.js
@@ -1,25 +1,66 @@
 import React, { Component } from 'react';
-import style from './switch-button.css';
+import styles from './switch-button.css';
 
 export default class SwitchButton extends Component {
   constructor(props) {
     super(props);
 
-    this.onChange = this.props.onChange.bind(this);
+    this.state = { hover: false, checked: props.checked };
+    this.onClick = this.onClick.bind(this);
+    this.mouseOver = this.mouseOver.bind(this);
+    this.mouseOut = this.mouseOut.bind(this);
+  }
+
+  onClick() {
+    this.setState({ checked: !this.state.checked });
+    this.props.onChange();
+  }
+
+  mouseOver() {
+    this.setState({ hover: true });
+  }
+
+  mouseOut() {
+    this.setState({ hover: false });
   }
 
   render() {
+    let backgroundStyle = styles.background;
+    let buttonStyle = styles.button;
+
+    if (this.state.checked) {
+      buttonStyle = Object.assign({}, buttonStyle, styles.buttonChecked);
+      if (this.state.hover) {
+        backgroundStyle = Object.assign({}, backgroundStyle, styles.backgroundHoverAndChecked);
+      } else {
+        backgroundStyle = Object.assign({}, backgroundStyle, styles.backgroundChecked);
+      }
+    } else if (this.state.hover) {
+      backgroundStyle = Object.assign({}, backgroundStyle, styles.backgroundHover);
+    }
+
     return (
-      <div className={this.props.className} style={style}>
-        <label htmlFor="backstage-switch-button">{this.props.label}</label>
+      <div className={this.props.className} style={styles.container}>
         <input
+          style={styles.checkbox}
           id="backstage-switch-button"
-          onChange={this.props.onChange}
-          checked={this.props.checked}
+          onChange={this.onChange}
+          checked={this.state.checked}
           name={this.props.name}
           type="checkbox"
           value="1"
         />
+        <div
+          style={backgroundStyle}
+          onClick={this.onClick}
+          onMouseOver={this.mouseOver}
+          onMouseOut={this.mouseOut}
+        >
+          <div style={buttonStyle} />
+        </div>
+        <label style={styles.label} htmlFor="backstage-switch-button">
+          {this.props.label}
+        </label>
       </div>
     );
   }
